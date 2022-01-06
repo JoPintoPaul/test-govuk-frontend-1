6493331c7b210cbda76b078f96458c68da55a708
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-01-06 12:48:13 +0000

    Spike investigation

--- build.sbt
+++ build.sbt
@@ -12,6 +12,7 @@ lazy val microservice = Project(appName, file("."))
     scalaVersion                     := "2.12.13",
     libraryDependencies              ++= AppDependencies.compile ++ AppDependencies.test,
     pipelineStages in Assets := Seq(gzip),
+    resolvers += Resolver.mavenLocal,
     // ***************
     // Use the silencer plugin to suppress warnings
     scalacOptions += "-P:silencer:pathFilters=routes",
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,12 +7,12 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.18.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.31.0-play-28"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.32.0-play-28-SNAPSHOT"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.18.0"             % Test,
-    
+
     "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
   )
